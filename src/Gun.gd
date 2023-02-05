4bf84e8006aef9b7b287e8d6dd73bdb10a811a6f
ArrowTheFighter <arrowthefighter@gmail.com>
2023-02-04 21:00:43 -0600

    Mushroom and attack Cooldown

--- src/Gun.gd
+++ src/Gun.gd
@@ -5,16 +5,30 @@ var flowerScn = preload("res://Scenes/SmallFlowerModel.tscn")
 onready var aim = $"../CameraFocus/SpringArm"
 onready var player = find_parent("Player")
 onready var world: Node = player.get_parent()
+var timeWhenLastFired = 0
+export var fireCooldown = 200
+export var fire2Cooldown = 1000
 
 var seed_velocity = 48.0
 
-func fire() -> void:
+func do_fire(ID) -> void:
+	match ID:
+		0:
+			if timeWhenLastFired + fireCooldown >= Time.get_ticks_msec():
+				return
+		_:
+			if timeWhenLastFired + fire2Cooldown >= Time.get_ticks_msec():
+				return
+				
+	timeWhenLastFired = Time.get_ticks_msec()
 	var new_seed = seed_scn.instance()
 	
 	world.add_child(new_seed)
 	new_seed.global_transform.origin = global_transform.origin
 	
 	new_seed.linear_velocity = player.linear_velocity
+	new_seed.SetItemToSpawnId(ID)
+	
 	
 	var basis = aim.global_transform.basis
 	var aim_vector: Vector3 = -basis.z.normalized()
@@ -23,6 +37,9 @@ func fire() -> void:
 	
 	new_seed.apply_central_impulse(aim_vector * seed_velocity)
 
+
 func _process(_delta):
-	if Input.is_action_just_pressed("fire1"):
-		fire()
+	if Input.is_action_pressed("fire1"):
+		do_fire(0)
+	if Input.is_action_pressed("fire2"):
+		do_fire(1)
--- src/Seed.gd
+++ src/Seed.gd
@@ -1,14 +1,16 @@
 extends RigidBody
 
-var grow_this = preload("res://Scenes/SmallFlowerModel.tscn")
-var grow_this2 = preload("res://Scenes/Thingy.tscn")
+var Thingy = preload("res://Scenes/Thingy.tscn")
+var grow_this = [preload("res://Scenes/SmallFlowerModel.tscn"),preload("res://Scenes/MushroomModel.tscn")]
+
+var itemToSpawn = 0
 
 func _on_Seed_body_entered(body):
-	var new = grow_this.instance()
+	var new = grow_this[itemToSpawn].instance()
 	get_parent().add_child(new)
 	new.global_transform.origin = global_transform.origin + (Vector3.UP * 1)
 	
-	var new2 = grow_this2.instance()
+	var new2 = Thingy.instance()
 	get_parent().add_child(new2)
 	new2.global_transform.origin = global_transform.origin + (Vector3.UP * 1)
 	
@@ -17,4 +19,7 @@ func _on_Seed_body_entered(body):
 
 func _on_DeathTimer_timeout():
 	queue_free()
+	
+func SetItemToSpawnId(ID):
+	itemToSpawn = ID
 
