0f81b6159570123b2298e2cf8a5f767d8afec674
ArrowTheFighter <arrowthefighter@gmail.com>
2023-02-05 10:36:52 -0600

    TankFire

--- Scenes/TankScript.gd
+++ Scenes/TankScript.gd
@@ -1,14 +1,19 @@
 extends Spatial
 
+var shootParticle = preload("res://TankParticleShoot.tscn")
 var dieParticle = preload("res://Scenes/ParticleScenes/TankDieParticle.tscn")
 var redMat = preload("res://assets/Materials/RedMat.tres")
 var BaseMat = preload("res://assets/Models/Material_010.material")
+
 var target
 
 var turnSpeed = 0.05
+var isLooking = true
 var enabled = true
 var TankHealth = 3
 var IAmTank = true
+var timeWhenLastFired = 0
+var FireCooldown = 2000
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,13 +24,34 @@ func _ready():
 func _process(delta):
 	if !enabled:
 		return
-	var newTarget = target.global_translation
-	newTarget.y = global_translation.y
-	$LookAt.look_at(newTarget,Vector3.UP)
-	global_transform = global_transform.interpolate_with($LookAt.global_transform,turnSpeed)
+	if isLooking:
+		var newTarget = target.global_translation
+		newTarget.y = global_translation.y
+		$LookAt.look_at(newTarget,Vector3.UP)
+		global_transform = global_transform.interpolate_with($LookAt.global_transform,turnSpeed)
+	
+	var in_range = $Area.get_overlapping_bodies()
+	
+	for body in in_range:
+		print(body)
+		if body == $KinematicBody:
+			continue
+		if timeWhenLastFired + FireCooldown < Time.get_ticks_msec():
+			Fire()
+		
 	
 	
+func Fire():
+	timeWhenLastFired = Time.get_ticks_msec()
+	var particle = shootParticle.instance()
+	get_parent().add_child(particle)
+	var offset = Vector3.UP * 2 + global_transform.basis.z * -1.6
+	particle.global_transform.origin = global_transform.origin + offset
+	particle.global_rotation.y = global_rotation.y
+	particle.scale = Vector3.ONE * 0.5
+	particle.emitting = true
 	
+	$AnimationPlayer.play("kickbakc")
 	
 func takeDamage(damage):
 	TankHealth -= damage
