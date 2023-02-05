c482be6169db5d4ecc2ed8ec00fd3cca3deb67fe
ArrowTheFighter <30394494+ArrowTheFighter@users.noreply.github.com>
2023-02-04 20:16:38 -0600

    Flower shrink (#4)

--- Scenes/SmallFlowerModel.gd
+++ Scenes/SmallFlowerModel.gd
@@ -8,13 +8,22 @@ var startScale
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	startScale = transform.basis
-	print(startScale)
+	startScale = scale
 	transform.basis = transform.basis.scaled(Vector3.ZERO)
 	var tween := create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
-	tween.tween_property(self, "transform:basis" ,startScale, 0.5)
+	tween.tween_property(self, "scale" ,startScale, 0.5)
 	pass # Replace with function body.
-
+	
+	tween.connect("finished", self, "shrink")
+	
+func shrink():
+	var tween := create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
+	tween.tween_property(self, "scale" ,Vector3.ZERO, 0.5)
+	
+	tween.connect("finished", self, "KillSelf")
+	
+func KillSelf():
+	queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
